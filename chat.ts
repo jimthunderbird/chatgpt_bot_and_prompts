fbfe92b30d9a3b9ac9fd50246d77e7a6cf3fe638
jim zike huang <jim.javathunderbird@gmail.com>
2023-03-03 09:23:39 -0800

    sample prompts

---
+++ chat.ts
@@ -0,0 +1,65 @@
+const fs = require('fs/promises');
+const { ChatGPTAPI } = require('chatgpt');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+class ChatGPTClient {
+  constructor(promptTemplate, apiKey, model, temperature, top_p) {
+    this.promptTemplate = promptTemplate;
+    this.parentMessageId = null;
+    this.api = new ChatGPTAPI({
+      apiKey,
+      completionParams: {
+        model,
+        temperature,
+        top_p
+      }
+    });
+  }
+
+  async init() {
+    const promptTemplateContent = await fs.readFile(this.promptTemplate, 'utf-8');
+    let messageToChatGPT = promptTemplateContent;
+
+    await this.sendMessage(messageToChatGPT);
+  }
+
+  async sendMessage(messageToChatGPT) {
+    let buffer = "";
+    const res = await this.api.sendMessage(messageToChatGPT, {
+      parentMessageId: this.parentMessageId,
+      onProgress: this.handleProgress.bind(this)
+    });
+    process.stdout.write("\n");
+    this.parentMessageId = res.id;
+  }
+
+  handleProgress(partialResponse) {
+    let buffer = this.buffer || "";
+    const bufferDiff = partialResponse.text.substr(buffer.length);
+    buffer = partialResponse.text;
+    process.stdout.write(bufferDiff);
+    this.buffer = buffer;
+  }
+
+  async handleInput(data) {
+    let messageToChatGPT = data.toString().trim();
+    await this.sendMessage(messageToChatGPT);
+  }
+}
+
+if (process.argv.length <= 2) {
+  console.log("Please provide a name of the prompt template");
+  process.exit(1);
+}
+
+const apiKey = process.env.OPENAI_API_KEY;
+const model = "gpt-3.5-turbo";
+const temperature = 0.5;
+const top_p = 0.8;
+
+const client = new ChatGPTClient(process.argv[2], apiKey, model, temperature, top_p);
+client.init();
+
+process.stdin.on('data', client.handleInput.bind(client));
